e5afe3dfe0a5d7d248c3908367f131d979ccc2dd
Prashant Srivastava <prashantsrivastava.knowledge@gmail.com>
2020-12-08 11:11:59 +0530

    Final Hosting on Firebase

--- shopomart/src/DataLayerConfig/Axios.js
+++ shopomart/src/DataLayerConfig/Axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-    baseURL: 'http://localhost:5001/shopomart-1b22e/us-central1/api'      /* The API (cloud function URL) */
+    baseURL: 'https://us-central1-shopomart-1b22e.cloudfunctions.net/api' // API from Firebase
+    // After firebase deploy --only functions
+    
+    // 'http://localhost:5001/shopomart-1b22e/us-central1/api'      /* The API (cloud function URL) */
 });
 
 export default Axios;
